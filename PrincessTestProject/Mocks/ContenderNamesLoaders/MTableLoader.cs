95b7da913d2f4caa7b496138a1c7553b89fa9067
OverKrice3000 <bober.xxl@yandex.ru>
2022-09-27 15:26:17 +0700

    added mock classes for testing

---
+++ PrincessTestProject/Mocks/ContenderNamesLoaders/MTableLoader.cs
@@ -0,0 +1,29 @@
+﻿using PrincessProject.utils.ContenderNamesLoader;
+
+namespace PrincessTestProject.Mocks.ContenderNamesLoaders;
+
+public class MSingleColumnLoader : ITableLoader
+{
+    private string _column;
+    private int _count;
+
+    public MSingleColumnLoader(string column, int count)
+    {
+        _column = column;
+        _count = count;
+    }
+
+    public Dictionary<string, List<string>> Load()
+    {
+        var valueBase = _column.ToUpper();
+        var list = new List<string>(_count);
+        for (int i = 0; i < _count; i++)
+        {
+            list.Add($"{valueBase}{i}");
+        }
+
+        var dictionary = new Dictionary<string, List<string>>(1);
+        dictionary.Add(_column, list);
+        return dictionary;
+    }
+}
