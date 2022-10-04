934d3b6d23308b444c26c236aa90684590715f7b
OverKrice3000 <bober.xxl@yandex.ru>
2022-10-04 23:44:42 +0700

    loaded migrations

---
+++ PrincessProject/ContenderGenerator/FromDatabaseContenderGenerator.cs
@@ -0,0 +1,32 @@
+﻿using PrincessProject.Data.context;
+using PrincessProject.model;
+
+namespace PrincessProject.ContenderGenerator;
+
+public class FromDatabaseContenderGenerator : IContenderGenerator
+{
+    private int _attemptId;
+    private AttemptContext _context;
+
+    public FromDatabaseContenderGenerator(AttemptContext context, int attemptId)
+    {
+        _context = context;
+        _attemptId = attemptId;
+    }
+
+    public Contender[] Generate(int size)
+    {
+        if (_context.Attempts.Find(_attemptId) is null)
+        {
+            throw new ArgumentException("No attempt with such id!");
+        }
+
+        return _context.AttemptData.Where(dto => dto.AttemptId == _attemptId).OrderBy(dto => dto.CandidateOrder)
+            .Select(dto => new Contender(dto.CandidateName, dto.CandidateSurname, dto.CandidateValue)).ToArray();
+    }
+
+    public void SetAttemptId(int attemptId)
+    {
+        _attemptId = attemptId;
+    }
+}
